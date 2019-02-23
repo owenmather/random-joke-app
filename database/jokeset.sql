1a14ac8844dad3e1837ccba0a61550f9697300c1
Owen <owenmather7@gmail.com>
2019-02-24 00:33:52 +0100

    Fix typo

---
+++ database/jokeset.sql
@@ -0,0 +1,479 @@
+USE `jokeapp`;
+INSERT INTO `joke`(description) VALUES
+('Q: How do you get the code for the bank vault?
+
+A: You checkout their branch.
+
+'),('
+
+Q: How did the developer announce their engagement?
+
+A: They `return`ed `true`!
+
+'),('
+
+Q: What do you call a busy waiter?
+
+A: A server.
+
+'),('
+
+Q: What do you call an idle server?
+
+A: A waiter.
+
+'),('
+
+```
+[Please Enter New Password]
+
+fortnight
+
+[Error: Password is Two Week]
+```
+
+'),('
+
+Q: How many Prolog programmers does it take to change a lightbulb?
+
+A: Yes.
+
+'),('
+
+I’ve been hearing news about this big boolean.
+
+Huge if true.
+
+'),('
+
+Q: What diet did the ghost developer go on?
+
+A: Boolean
+
+'),('
+
+Q: Why was the developer unhappy at their job?
+
+A: They wanted arrays.
+
+'),('
+
+Q: Why did 10 get paid less than 10?
+
+A: There was workplace inequality.
+
+'),('
+
+Q: Why was the function sad after a successful first call?
+
+A: It didn’t get a callback.
+
+'),('
+
+Q: Why did the angry function exceed the callstack size?
+
+A: It got into an Argument with itself
+
+'),('
+
+Q: Whats the object-oriented way to become wealthy?
+
+A: Inheritance
+
+'),('
+
+Q: Why did the developer ground their kid?
+
+A: They werent telling the truthy
+
+'),('
+
+Q: What did the array say after it was extended?
+
+A: Stop objectifying me.
+
+'),('
+
+!false
+Its funny cause its true.
+'),('
+Q: Where did the parallel function wash its hands?
+A: Async
+'),('
+
+Q: Why did the functions stop calling each other?
+
+A: Because they had constant arguments.
+
+'),('
+
+Q: Whats the second movie about a database engineer called?
+
+A: The SQL.
+
+'),('
+
+A programmers significant other tells them, Run to the store and pick up a loaf of bread. If they have eggs, get a dozen.
+
+The programmer comes home with 12 loaves of bread.
+
+'),('
+
+Q: What did the spider do on the computer?
+
+A: Made a website!
+
+'),('
+
+Q: What did the computer do at lunchtime?
+
+A: Had a byte!
+
+'),('
+
+Q: What does a baby computer call his father?
+
+A: Data!
+
+'),('
+
+Q: Why did the computer keep sneezing?
+
+A: It had a virus!
+
+'),('
+
+Q: What is a computer virus?
+
+A: A terminal illness!
+
+'),('
+
+Q: Why was the computer cold?
+
+A: It left its Windows open!
+
+'),('
+
+Q: Why was there a bug in the computer?
+
+A: Because it was looking for a byte to eat?
+
+'),('
+
+Q: Why did the computer squeak?
+
+A: Because someone stepped on its mouse!
+
+'),('
+
+Q: What do you get when you cross a computer and a life guard?
+
+A: A screensaver!
+
+'),('
+
+Q: Where do all the cool mice live?
+
+A: In their mousepads!
+
+'),('
+
+Q: What do you get when you cross a computer with an elephant?
+
+A: Lots of memory!
+
+'),('
+
+Java truly is an OOP language...
+
+As in: OOPs I used Java!
+
+'),('
+
+Q: How do programming pirates pass method parameters?
+
+A: Varrrrarrrgs.
+
+'),('
+
+Q: How do programming shepherds count their flock?
+
+A: With lambda functions
+
+'),('
+
+Q: How did pirates collaborate before computers ?
+
+A: Pier to pier networking.
+
+'),('
+
+Q: Why dont bachelors like Git?
+
+A: Because they are afraid to commit.
+
+'),('
+
+Q: A SQL query goes into a bar, walks up to two tables and asks:
+
+A: Can I JOIN you?
+
+'),('
+
+Q: How does a developer make a cheer?
+
+A: [hip,hip] // (hip hip array!)
+
+'),('
+
+Q: Why was the developers family upset with them at dinner?
+
+A: They forgot to git squash before going home
+
+'),('
+
+Q: What did JavaScript call his son?
+
+A: JSON!
+
+'),('
+
+Q: What did the proud React component say to its child?
+
+A: Ive got to give you props
+
+'),('
+
+Q: What did the server say to his client who was having a bad day?
+
+A: Everythings going to be 200
+
+'),('
+
+Q: Why did the developer go broke?
+
+A: Because they used up all their cache
+
+'),('
+
+Q: Are computers dangerous?
+
+A: Nah, they dont byte. They just nibble a bit.
+
+'),('
+
+Q: How did the mafioso kill the Node server?
+
+A: Tie await to it and let it async.
+
+'),('
+
+Q: You know what the best thing about booleans is?
+
+A: Even if you are wrong, you are only off by a bit.
+
+'),('
+
+Q: What do you do when you cant understand your husbands behavior?
+
+A: man man
+
+'),('
+
+Q: How many developers does it take to change a light bulb?
+
+A: None. Its a hardware issue
+
+'),('
+
+Q: Why do programmers always mix up Halloween and Christmas?
+
+A: Because 31 OCT == 25 DEC
+
+'),('
+
+Q: What are computers favorite snacks?
+
+A: Microchips, phish sticks, and cookies. But just a few bytes of each.
+
+'),('
+
+Q: Whats a compiler developers favorite spice?
+
+A: Parsley.
+
+'),('
+
+Q: A SQL developer walked into a NoSQL bar.
+
+A: They left because they couldnt find a table.
+
+'),('
+
+Q: How do you help JS errors?
+
+A: You `console` them!
+
+'),('
+
+Q: Why dont parents teach their kids about regular expressions?
+
+A: Because they dont want them playing with matches
+
+'),('
+
+Q: Why didnt the div` get invited to the dinner party?
+
+A: Because it had no class!
+
+'),('
+
+Q: Why did the constant break up with the variable?
+
+A: Because they changed.
+
+'),('
+
+Q: Why did the database administrator leave his wife?
+
+A: She had one-to-many relationships.
+
+'),('
+
+Asynchronous JavaScript is amazing.
+
+I Promise you.
+
+'),('
+
+Q: Why do Java developers wear glasses?
+
+A: Because they dont C#!
+
+'),('
+
+Q: What are the three hardest problems in computer science?
+
+A: Naming things and off-by-one errors
+
+'),('
+
+Q: What did the fruit basket say to the developer?
+
+A: I hope youre ready for some pear programming!
+
+'),('
+
+Ive got a great UDP joke but Im afraid you wouldnt get it...
+
+'),('
+
+A programmer was arrested for writing unreadable code. They refused to comment.
+
+'),('
+
+There are 10 types of people in this world, those who understand binary and those who dont.
+
+'),('
+
+Q: Why do all HTML emails get blocked?
+
+A: Because they are all `<span />`.
+
+'),('
+
+Q: What did the process say after working in an infinite loop all day?
+
+A: I need a break.
+
+'),('
+
+Q: An Agent died unexpectedly. How was the crime solved?
+
+A: By looking at the Stack Trace.
+
+'),('
+
+Q: Why did the document store go out of business?
+
+A: It had NoSQL.
+
+'),('
+
+Q: Why is Python like the Soviet Union?
+
+A: Because it has no private fields
+
+'),('
+
+Q: Where did the API go to eat?
+
+A: To the RESTaurant
+
+'),('
+
+Q: Why shouldnt you trust Matlab developers?
+
+A: Because theyre always plotting something.
+
+'),('
+
+Q: Why did the developer have to quit smoking?
+
+A: Because they couldnt afford to pay the new syntax.
+
+'),('
+
+Q: How does a programmer open a jar for their significant other?
+
+A: They install Java
+
+'),('
+
+Q: What did the psychic say to the developers?
+
+A: I see dev people.
+
+'),('
+
+Q: Where does the pirate stash all of their digital treasures?
+
+A: RAR
+
+'),('
+
+Q: What is Reacts favorite movie genre?
+
+A: Suspense
+
+'),('
+
+Q: Why couldnt the React component understand the joke?
+
+A: Because it didnt get the context.
+
+'),('
+
+Q: What did XHR say to AJAX when it thought it was being a Mean Girl?
+
+A: Stop trying to make fetch happen!
+
+'),('
+
+ Q: What was Grace Hoppers favorite car?
+
+ A: VW Bug
+
+'),('
+
+Q: What sits on a pirates shoulder and calls, Pieces of seven, Pieces of seven
+
+A: Parroty error.
+
+'),('
+
+Q: Why did the programmer come home crying?
+
+A: His friends were always boolean him
+')
