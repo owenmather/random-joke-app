32ffe93546f0380a3eae719a777dda3f59eedccf
Owen <owenmather7@gmail.com>
2019-02-23 22:15:19 +0100

    Fix typo

--- src/main/java/com/mather/inventions/jokeapp/fact/FactCrudRepository.java
+++ src/main/java/com/mather/inventions/jokeapp/fact/FactCrudRepository.java
@@ -1,6 +1,8 @@
 package com.mather.inventions.jokeapp.fact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="fact")
 public interface FactCrudRepository extends JpaRepository<Fact,Integer> {
 }
